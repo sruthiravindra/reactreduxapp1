4c2fa784a35787cd937e964cca65772c04fcd962
sruthiravindra <sruthi.ravindran@gmail.com>
2023-07-12 13:02:47 +0400

    AnimatedDisplayCard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
-import { Counter } from './features/counter/Counter';
-import CampsiteCard from './features/campsites/CampsiteCard.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -16,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
+        <Route path='about' element={<AboutPage/>}></Route>
         <Route path='contact' element={<ContactPage/>}></Route>
         <Route path='directory' element={<CampsitesDirectoryPage/>}></Route>
         <Route path='directory/:campsiteId' element={<CampsiteDetailPage/>}></Route>
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,20 +1,24 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from './DisplayCard';
+//import DisplayCard from './DisplayCard';
 import { selectFeaturdPromotions } from "../promotions/promotionsSlice";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
+import { selectFeaturedPartner } from "../partners/partnersSlice";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 
 const DispayList = () => {
-    const items = [selectFeaturdPromotions(), selectFeaturedCampsite()]
-    return(
-            <Row>
-                {items.map((item, idx)=>{
-                    return(
+    const items = [selectFeaturdPromotions(), selectFeaturedCampsite(), selectFeaturedPartner()]
+    return (
+        <Row>
+            {items.map((item, idx) => {
+                return (
+                    item && (
                         <Col md className="m-1" key={idx}>
-                            <DisplayCard item={item}/>
+                            <AnimatedDisplayCard item={item} />
                         </Col>
                     )
-                })}
-            </Row>
+                )
+            })}
+        </Row>
     );
 }
 
